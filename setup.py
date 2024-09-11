f642ae1707b1162c362467daa2e53f92b7604345
WildRikku <119439592+WildRikku@users.noreply.github.com>
2024-09-11 14:42:36 +0000

    bump version and change author and repository information as this repository has been taken over

--- setup.py
+++ setup.py
@@ -15,20 +15,20 @@ plugin_name = "OctoPrint-SpoolManager"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 #
-plugin_version = "1.7.3"
+plugin_version = "1.7.4"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = """Plugin to manage Spools"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "dojohnso"
+plugin_author = "WildRikku"
 
 # The plugin's author's mail address.
-plugin_author_email = "doug@djohnson.tech"
+plugin_author_email = "octoprintspoolmanager@wildrikku.de"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/dojohnso/OctoPrint-SpoolManager"
+plugin_url = "https://github.com/WildRikku/OctoPrint-SpoolManager"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
